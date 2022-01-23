92933c435650f642de12efc04717208b124cbe9a
h.yoshino <osushicrusher@gmail.com>
2022-01-23 23:01:03 +0900

    Remove unnecessary code, set tailwindcss purge

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,11 @@ import App from './App.vue'
 import './index.css'
 import { createPinia } from "pinia"
 import router from './router/index'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faPlay, faSearch, faTimesCircle, faMusic, faChevronDown } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+// import { library } from '@fortawesome/fontawesome-svg-core'
+// import { faPlay, faSearch, faTimesCircle, faMusic, faChevronDown } from '@fortawesome/free-solid-svg-icons'
+// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faPlay, faSearch, faTimesCircle, faMusic, faChevronDown)
+// library.add(faPlay, faSearch, faTimesCircle, faMusic, faChevronDown)
+// component('fa', FontAwesomeIcon )
 
-createApp(App).component('fa', FontAwesomeIcon ).use(router).use(createPinia()).mount("#app")
+createApp(App).use(router).use(createPinia()).mount("#app")
