1d1cdd68cf606d62f6558c55862aa66436407d57
h.yoshino <osushicrusher@gmail.com>
2022-01-29 21:50:30 +0900

    modified viteconfig

--- src/components/modules/Footer.vue
+++ src/components/modules/Footer.vue
@@ -2,9 +2,9 @@
     <footer class="text-gray-600 body-font">
         <div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
         <span class="inline-flex items-center sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-            <a href="https://twitter.com/oichiiosushi" rel="noopener noreferrer" target="_blank" class="mr-3">
+            <!-- <a href="https://twitter.com/oichiiosushi" rel="noopener noreferrer" target="_blank" class="mr-3">
                 <img src="/src/assets/icons/Twitter/twitter.png" width="32px" alt="twitterのアイコン">
-            </a>
+            </a> -->
             <a href="https://github.com/osushicrusher" rel="noopener noreferrer" target="_blank" class="mr-3">
                 <img src="/src/assets/icons/GitHub/PNG/GitHub-Mark-32px.png" width="32px" alt="Githubのアイコン">
             </a>
