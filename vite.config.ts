7ee28f258970c8e5981f25651b33b0ce7c32f65d
h.yoshino <osushicrusher@gmail.com>
2022-01-29 20:07:34 +0900

    チャンクする粒度を細かくする

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import compress from 'vite-plugin-compress'
 // import ViteWebp from 'vite-plugin-vue-webp'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-  ]
+    compress(),
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules'))
+            return id.toString().split('node_modules/')[1].split('/')[0].toString()
+        },
+      }
+    }
+  }
 })
