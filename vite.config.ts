2baa111d82e572daf68d074e5996dbf95bb48206
h.yoshino <osushicrusher@gmail.com>
2022-01-29 23:22:19 +0900

    modified viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -20,6 +20,7 @@ export default defineConfig({
       // external: ['vue', 'vue-demi'],
     },
   },
+  base: './'
   // resolve: {
   //   alias: [{ find: 'assets', replacement: 'src/assets'}]
   // }
