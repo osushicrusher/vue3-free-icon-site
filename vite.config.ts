f7d54eaef9108909059ccf61737392e1e1defb37
h.yoshino <osushicrusher@gmail.com>
2022-01-29 22:29:28 +0900

    modified viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -12,10 +12,10 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        manualChunks(id) {
-          if (id.includes('node_modules'))
-            return id.toString().split('node_modules/')[1].split('/')[0].toString()
-        },
+        // manualChunks(id) {
+        //   if (id.includes('node_modules'))
+        //     return id.toString().split('node_modules/')[1].split('/')[0].toString()
+        // },
       },
       // external: ['vue', 'vue-demi'],
     },
