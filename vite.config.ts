0304f9dc944add4fda73182f1d76e2f6e7aea285
h.yoshino <osushicrusher@gmail.com>
2022-01-29 21:47:00 +0900

    modified viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -9,14 +9,14 @@ export default defineConfig({
     vue(),
     compress(),
   ],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules'))
-            return id.toString().split('node_modules/')[1].split('/')[0].toString()
-        },
-      }
-    }
-  }
+  // build: {
+  //   rollupOptions: {
+  //     output: {
+  //       manualChunks(id) {
+  //         if (id.includes('node_modules'))
+  //           return id.toString().split('node_modules/')[1].split('/')[0].toString()
+  //       },
+  //     }
+  //   }
+  // }
 })
